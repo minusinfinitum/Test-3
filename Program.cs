b88bc0de491fa8dbcc9070282b5752a2c1c0c638
minusinfinitum <155751627+minusinfinitum@users.noreply.github.com>
2024-01-06 10:03:49 +0300

    Teach Comparison

--- Program.cs
+++ Program.cs
@@ -13,3 +13,10 @@ Console.WriteLine($"Tushunarli demak  {age}");
 int test3Age = 5;
 int ageDiccerence =  age - test3Age;
 Console.WriteLine($"O'zing bilan test 3 ni  farqini korsat {ageDiccerence}");
+
+System.Console.WriteLine($"Kescha bilan tengmisan: {age >= test3Age }");
+System.Console.WriteLine($"Keschadan yoshmidan:{age < test3Age }");
+System.Console.WriteLine($"Kescha bian teng emasmisan: {age != test3Age}");
+System.Console.WriteLine($"Kescha sendan kattami: {age > test3Age}");
+System.Console.WriteLine($"Kescha sendan kichik yoki tengmi: {age <= test3Age}");
+System.Console.WriteLine($"Kescha sen bilan tengmi: {age == test3Age}");
