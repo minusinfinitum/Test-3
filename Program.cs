0535bbfd9b0b7a78fe7e03d4bf53a63095646505
minusinfinitum <155751627+minusinfinitum@users.noreply.github.com>
2024-01-06 11:12:01 +0300

    Teach Comparison

--- Program.cs
+++ Program.cs
@@ -1,21 +1,9 @@
-﻿Console.Write("Qalesan: ");
-string name = Console.ReadLine();
-string greeting = $"Hi, {name}";
-Console.WriteLine(greeting);
+﻿using System;
 
-Console.Write("Hozir nech yoshdasan: ");
-string ageAsString = Console.ReadLine();
-Console.WriteLine("Davom etyapdi...");
-int age = Convert.ToInt32(ageAsString);
-Console.WriteLine($"Tushunarli demak {age}");
+Console.Write("Radiusni kiriting: ");
+double radius = Convert.ToDouble(Console.ReadLine());
 
-int test3Age = 5;
-int ageDifference = age - test3Age;
-Console.WriteLine($"O'zing bilan test 3 ni farqini korsat {ageDifference}");
+double area = Math.PI * Math.Pow(radius, 2);
+double circumference = 2 * Math.PI * radius;
 
-System.Console.WriteLine($"Kescha bilan tengmisan: {age >= test3Age }");
-System.Console.WriteLine($"Keschadan yoshmidan:{age < test3Age }");
-System.Console.WriteLine($"Kescha bian teng emasmisan: {age != test3Age}");
-System.Console.WriteLine($"Kescha sendan kattami: {age > test3Age}");
-System.Console.WriteLine($"Kescha sendan kichik yoki tengmi: {age <= test3Age}");
-System.Console.WriteLine($"Kescha sen bilan tengmi: {age == test3Age}");
+Console.WriteLine($"S= {area}, L= {circumference}");
