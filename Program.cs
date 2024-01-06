76e59ba275e57200338353dd0b3b43319b697aea
minusinfinitum <155751627+minusinfinitum@users.noreply.github.com>
2024-01-06 11:14:01 +0300

    Teach Comparison

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿// radius
+
+using System;
 
 Console.Write("Radiusni kiriting: ");
 double radius = Convert.ToDouble(Console.ReadLine());
@@ -7,3 +9,31 @@ double area = Math.PI * Math.Pow(radius, 2);
 double circumference = 2 * Math.PI * radius;
 
 Console.WriteLine($"S= {area}, L= {circumference}");
+
+
+// qiymat 
+
+using System;
+
+Console.Write("Qiymatni kiriting: ");
+double qiymat = Convert.ToDouble(Console.ReadLine());
+
+Console.Write("Kursni kiriting: ");
+double kurs = Convert.ToDouble(Console.ReadLine());
+double natija = qiymat * kurs;
+
+Console.WriteLine($"Natija: {natija} so'm");
+    
+    
+
+// yosh 
+using System;
+
+Console.Write("Tug'ilgan yilni kiriting: ");
+
+int tugilganYil = Convert.ToInt32(Console.ReadLine());
+
+int joriyYil = DateTime.Now.Year;
+int yosh = joriyYil - tugilganYil;
+
+Console.WriteLine($"Sizning yoshingiz: {yosh * 365} kun");
