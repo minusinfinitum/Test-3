c605847e6d82861d9fcc28cd5246393309507b64
minusinfinitum <155751627+minusinfinitum@users.noreply.github.com>
2024-01-12 22:25:48 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -15,3 +15,31 @@ class Program
         Console.WriteLine(result);
     }
 }
+
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        int x = 15;
+        int y = 18;
+        string result;
+
+        if (x > y)
+        {
+            result = "x is greater than y";
+        }
+        else if (x == y)
+        {
+            result = "x is equal to y";
+        }
+        else
+        {
+            result = "x and y are not comparable";
+        }
+
+        Console.WriteLine(result);
+    }
+}
