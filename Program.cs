9e2a16676ecdb86f7fb9bb915ae0840e146975f0
minusinfinitum <155751627+minusinfinitum@users.noreply.github.com>
2024-01-12 21:28:17 +0300

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -33,3 +33,22 @@ else
     System.Console.WriteLine("Siz yoshsiz");
 }
 
+
+
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("X qiymati : ");
+        int x = Convert.ToInt32(Console.ReadLine());
+
+        Console.Write("Str: ");
+        string str = Console.ReadLine();
+
+        string result = x > str.Length ? str.ToUpper() : str.ToLower();
+
+        Console.WriteLine(result);
+    }
+}
